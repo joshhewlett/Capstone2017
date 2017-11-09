c6a641a2c1fb69c59ced2ed06b30586659b44e30
Josh Hewlett <jhewlett@asu.edu>
2017-11-08 17:20:16 -0700

    Slide update

---
+++ server/db/mysql/slide/update.js
@@ -0,0 +1,19 @@
+import executeQuery from '../query';
+import {
+  DBConfig
+} from "../config";
+
+var mysql = require('mysql');
+var table = DBConfig.dbs.slide;
+
+export default {
+  byId: async(id, slide) => {
+    if (!id || !slide.sequence) {
+      throw "Missing attribute on slide";
+    }
+    var sql = "UPDATE " + table + " SET email = ? WHERE id = ?"
+    var inserts = [slide.sequence, id];
+    sql = mysql.format(sql, inserts);
+    return executeQuery(sql);
+  }
+};
