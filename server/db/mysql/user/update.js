afdbc0fb3e5f46af7e745fcbaf4c98e1adb622eb
Josh Hewlett <jhewlett@asu.edu>
2017-11-08 17:22:25 -0700

    User update change

--- server/db/mysql/user/update.js
+++ server/db/mysql/user/update.js
@@ -1,19 +1,19 @@
 import executeQuery from '../query';
 import {
-    DBConfig
+  DBConfig
 } from "../config";
 
 var mysql = require('mysql');
 var table = DBConfig.dbs.user;
 
 export default {
-    byId: async(id, user) => {
-        if (!id || !user.email) {
-            throw "Missing attribute on user";
-        }
-        var sql = "UPDATE " + table + " SET email = ? WHERE id = ?"
-        var inserts = [user.email, user.id];
-        sql = mysql.format(sql, inserts);
-        return executeQuery(sql);
+  byId: async(id, user) => {
+    if (!id || !user.email) {
+      throw "Missing attribute on user";
     }
+    var sql = "UPDATE " + table + " SET email = ? WHERE id = ?"
+    var inserts = [user.email, id];
+    sql = mysql.format(sql, inserts);
+    return executeQuery(sql);
+  }
 };
