e89e9af106ccbdcc78e13904c84b887c1ed190d4
Josh Hewlett <jhewlett@asu.edu>
2017-11-13 19:47:27 -0700

    Upload function works. Added @std/esm

--- server/db/s3/index.js
+++ server/db/s3/index.js
@@ -1,47 +1,44 @@
-import {
-    DBConfig
-} from '../config';
-
-let AWS = require('aws-sdk'),
-  fs = require('fs'),
-  path = require('path');
+import DBConfig from '../config';
+import AWS from 'aws-sdk';
+import fs from 'fs';
+import path from 'path';
 
 
 AWS.config.update({
-    accessKeyId: DBConfig.s3.accessKeyId,
-    secretAccessKey: DBConfig.s3.secretAccessKey,
-    region: DBConfig.s3.region
+  accessKeyId: DBConfig.s3.accessKeyId,
+  secretAccessKey: DBConfig.s3.secretAccessKey,
+  region: DBConfig.s3.region
 });
 
 let s3 = new AWS.S3();
 
-// Bucket names must be unique across all S3 users
+let myBucket = DBConfig.s3.bucket;
+let myKey = DBConfig.s3.key;
 
-var myBucket = DBConfig.s3.bucket;
-
-var myKey = DBConfig.s3.key;
-
-var uploadParams = {
-    Bucket: myBucket,
-    Key: myKey,
-    Body: ''
-};
-
-let file = '/home/james/GitHub/Capstone2017/server/db/s3/index.js';
+let file = '/Users/joshhewlett/Documents/Capstone/Capstone2017/server/db/s3/index.js';
 
 let fileStream = fs.createReadStream(file);
 fileStream.on('error', function(err) {
   console.log('File Error', err);
 });
-uploadParams.Body = fileStream;
-uploadParams.Key = path.basename(file);
 
-// call S3 to retrieve upload file to specified bucket
-s3.upload(uploadParams, function(err, data) {
-  if (err) {
-    console.log("Error", err);
-  }
-  if (data) {
-    console.log("Upload Success", data.Location);
-  }
-});
+upload(fileStream, "index", ".js");
+
+function upload(fileStream, path, fileExt) {
+  let key = path + fileExt;
+  console.log(DBConfig.s3.bucket);
+  let uploadParams = {
+    Bucket: DBConfig.s3.bucket,
+    Key: key,
+    Body: fileStream
+  };
+  console.log(uploadParams);
+  s3.upload(uploadParams, (err, data) => {
+    if (err) {
+      throw ("S3 Error: ", err);
+    }
+    console.log("Upload success");
+    return data;
+  })
+
+}
