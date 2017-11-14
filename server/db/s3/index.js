6a4624879e0ff5c2344c355b2b2f0323a788e634
Josh Hewlett <jhewlett@asu.edu>
2017-11-13 17:24:10 -0700

    Changes

--- server/db/s3/index.js
+++ server/db/s3/index.js
@@ -1,45 +1,41 @@
-var AWS = require('aws-sdk');
+let AWS = require('aws-sdk'),
+  fs = require('fs'),
+  path = require('path');
+
 
 AWS.config.update({
-    accessKeyId: "AKIAJZQZ2ZAXPHXPQAYQ",
-    secretAccessKey: "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",
-    region: 'us-east-2'
+  accessKeyId: "AKIAJZQZ2ZAXPHXPQAYQ",
+  secretAccessKey: "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",
+  region: 'us-east-2'
 });
 
-var s3 = new AWS.S3();
+let s3 = new AWS.S3();
 
 // Bucket names must be unique across all S3 users
 
-var myBucket = 'capstone-team3';
-
-var myKey = 'ea617e859e882e8e3def99ce87656473cd3f2e841ff8d2c4515d3a08be11c7f2';
-
-var uploadParams = {
-    Bucket: myBucket,
-    Key: myKey,
-    Body: ''
+let bucketName = 'capstone-team3';
+let bucketKey = 'ea617e859e882e8e3def99ce87656473cd3f2e841ff8d2c4515d3a08be11c7f2';
+let uploadParams = {
+  Bucket: bucketName,
+  Key: bucketKey,
+  Body: ''
 };
 
-var fs = require('fs');
+let file = '/home/james/GitHub/Capstone2017/server/db/s3/index.js';
 
-
-var file = '/home/james/GitHub/Capstone2017/server/db/s3/index.js';
-
-var fileStream = fs.createReadStream(file);
+let fileStream = fs.createReadStream(file);
 fileStream.on('error', function(err) {
-    console.log('File Error', err);
+  console.log('File Error', err);
 });
 uploadParams.Body = fileStream;
-
-var path = require('path');
 uploadParams.Key = path.basename(file);
 
 // call S3 to retrieve upload file to specified bucket
 s3.upload(uploadParams, function(err, data) {
-    if (err) {
-        console.log("Error", err);
-    }
-    if (data) {
-        console.log("Upload Success", data.Location);
-    }
+  if (err) {
+    console.log("Error", err);
+  }
+  if (data) {
+    console.log("Upload Success", data.Location);
+  }
 });
