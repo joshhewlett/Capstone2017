286601f037f02efc78939a4358b1d8b8b03d7ffe
Josh Hewlett <jhewlett@asu.edu>
2017-11-13 20:15:00 -0700

    Cleanup on index.js

--- server/db/s3/index.js
+++ server/db/s3/index.js
@@ -3,7 +3,6 @@ import fs from 'fs';
 import path from 'path';
 import DBConfig from '../config';
 
-
 AWS.config.update({
   accessKeyId: DBConfig.s3.accessKeyId,
   secretAccessKey: DBConfig.s3.secretAccessKey,
@@ -11,25 +10,13 @@ AWS.config.update({
 });
 
 let s3 = new AWS.S3();
-
 let myBucket = DBConfig.s3.bucket;
 let myKey = DBConfig.s3.key;
 
-let file = '/Users/joshhewlett/Documents/Capstone/Capstone2017/server/db/s3/index.js';
-
-let fileStream = fs.createReadStream(file);
-fileStream.on('error', function(err) {
-  console.log('File Error', err);
-});
-
-upload(fileStream, "index", ".js");
-
-function upload(fileStream, path, fileExt) {
-  let key = path + fileExt;
-  console.log(DBConfig.s3.bucket);
+async upload(fileStream, path, fileExt) {
   let uploadParams = {
     Bucket: DBConfig.s3.bucket,
-    Key: key,
+    Key: path + fileExt,
     Body: fileStream
   };
   console.log(uploadParams);
@@ -39,6 +26,5 @@ function upload(fileStream, path, fileExt) {
     }
     console.log("Upload success");
     return data;
-  })
-
+  });
 }
