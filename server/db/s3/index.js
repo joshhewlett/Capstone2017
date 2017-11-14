97c8bca6f7898924f60267fbc954050766d2e630
James Quigley <James.A.Quigley@asu.edu>
2017-11-13 20:33:03 -0700

    Download S3 function

--- server/db/s3/index.js
+++ server/db/s3/index.js
@@ -4,27 +4,41 @@ import path from 'path';
 import DBConfig from '../config';
 
 AWS.config.update({
-  accessKeyId: DBConfig.s3.accessKeyId,
-  secretAccessKey: DBConfig.s3.secretAccessKey,
-  region: DBConfig.s3.region
+    accessKeyId: DBConfig.s3.accessKeyId,
+    secretAccessKey: DBConfig.s3.secretAccessKey,
+    region: DBConfig.s3.region
 });
 
 let s3 = new AWS.S3();
 let myBucket = DBConfig.s3.bucket;
 let myKey = DBConfig.s3.key;
 
-async upload(fileStream, path, fileExt) {
-  let uploadParams = {
-    Bucket: DBConfig.s3.bucket,
-    Key: path + fileExt,
-    Body: fileStream
-  };
-  console.log(uploadParams);
-  s3.upload(uploadParams, (err, data) => {
-    if (err) {
-      throw ("S3 Error: ", err);
-    }
-    console.log("Upload success");
-    return data;
-  });
+function upload(fileStream, path) {
+    return new Promise((resolve, reject) => {
+        let uploadParams = {
+            Bucket: DBConfig.s3.bucket,
+            Key: path,
+            Body: fileStream
+        };
+        s3.upload(uploadParams, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    });
+}
+
+function download(path) {
+    return new Promise((resolve, reject) => {
+        s3.getObject({
+            Bucket: DBConfig.s3.bucket,
+            Key: path
+        }, (err, data) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(data);
+        });
+    })
 }
