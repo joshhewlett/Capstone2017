8695690da62e0081a9cc34621cd8a98d126c3332
James Quigley <James.A.Quigley@asu.edu>
2017-11-16 17:04:42 -0700

    Added some basic middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,36 @@
 // Loads development environment variables from Capstone2017/server/.env file
 import dotenv from 'dotenv'
 if (process.env.NODE_ENV !== 'production') {
-  dotenv.load();
+    dotenv.load();
 }
+
+/**
+ * Node module imports
+ */
+import express from 'express';
+import http from 'http';
+import compression from 'compression';
+import helmet from 'helmet';
+import bodyParser from 'body-parser';
+
+
+/**
+ * Project imports
+ */
+
+
+
+/**
+ * Server starts here
+ */
+var app = express();
+
+/**
+ * Configure Middleware
+ */
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
