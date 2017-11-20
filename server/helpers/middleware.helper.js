0b9b1053cbee8c16f3c6898d2c6863a91bd93156
James Quigley <James.A.Quigley@asu.edu>
2017-11-20 16:01:08 -0700

    Session middleware

--- server/helpers/middleware.helper.js
+++ server/helpers/middleware.helper.js
@@ -4,6 +4,7 @@
 import compression from 'compression';
 import helmet from 'helmet';
 import bodyParser from 'body-parser';
+import session from 'express-session';
 
 export default (app) => {
     app.logger.debug("Middleware Initialized");
@@ -12,5 +13,17 @@ export default (app) => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({
         extended: false
+    }));
+    app.use(session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        name: "session",
+        rolling: true,
+        cookie: {
+            httpOnly: true,
+            maxAge: process.env.SESSION_MAX_AGE,
+            secure: false
+        }
     }));
 }
