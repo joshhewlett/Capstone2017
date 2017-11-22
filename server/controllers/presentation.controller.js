87182966feba87c37e75e33e00083e57e6c0b7b2
Josh Hewlett <jhewlett@asu.edu>
2017-11-22 16:35:47 -0700

    Created Presentation function layout

--- server/controllers/presentation.controller.js
+++ server/controllers/presentation.controller.js
@@ -6,30 +6,49 @@ export default class extends BaseController {
 
     // Create a new Presentation object
     this.router.post('/', (req, res) => {
-
+      this.createPresentation(req, res);
     });
 
     // Returns all the slides belonging to Presentation
     // with the id of :id
     this.router.get('/:id/slides', (req, res) => {
-
+      this.getSlides(req, res);
     });
 
     // Returns object for Presentation with the id of :id
     this.router.get('/:id', (req, res) => {
-
+      this.getPresentation(req, res);
     });
 
     // Updates Presentation with id of :id
     this.router.put('/:id', (req, res) => {
-
+      this.updatePresentation(req, res);
     });
 
     // Deletes Presentation with id of :id
     this.router.delete('/:id', (req, res) => {
-
+      this.deletePresentation(req, res);
     });
 
   }
 
+  async createPresentation(req, res){
+
+  }
+
+  async getSlides(req, res){
+
+  }
+
+  async getPresentation(req, res){
+
+  }
+
+  async updatePresentation(req, res){
+
+  }
+
+  async deletePresentation(req, res){
+
+  }
 }
