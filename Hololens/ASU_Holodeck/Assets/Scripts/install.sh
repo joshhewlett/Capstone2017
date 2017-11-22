6d13b9f3a35c56c557de1614ac776fa0c35fc265
nshantz <nshantz@asu.edu>
2017-11-22 14:41:45 -0700

    Changed Install path from .app to .pkg

--- Hololens/ASU_Holodeck/Assets/Scripts/install.sh
+++ Hololens/ASU_Holodeck/Assets/Scripts/install.sh
@@ -4,8 +4,8 @@
 
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
-echo 'Downloading from https://unity3d.com/get-unity/download?thank-you=update&download_nid=48329&os=Mac: '
-curl -o Unity.pkg 'https://unity3d.com/get-unity/download?thank-you=update&download_nid=48329&os=Mac'
+echo 'Downloading from https://download.unity3d.com/download_unity/cc85bf6a8a04/MacEditorInstaller/Unity-2017.1.2f1.pkg?_ga=2.157252923.1270817159.1511310980-1180231831.1510707011: '
+curl -o Unity.pkg 'https://download.unity3d.com/download_unity/cc85bf6a8a04/MacEditorInstaller/Unity-2017.1.2f1.pkg?_ga=2.157252923.1270817159.1511310980-1180231831.1510707011'
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
