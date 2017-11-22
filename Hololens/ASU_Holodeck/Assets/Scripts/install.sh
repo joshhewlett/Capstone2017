6778dbf48c801d15f1878af83fa2317d39da6221
nshantz <nshantz@asu.edu>
2017-11-21 18:23:03 -0700

    Attempting to fix travis Unity install issue

--- Hololens/ASU_Holodeck/Assets/Scripts/install.sh
+++ Hololens/ASU_Holodeck/Assets/Scripts/install.sh
@@ -5,7 +5,7 @@
 # This link changes from time to time. I haven't found a reliable hosted installer package for doing regular
 # installs like this. You will probably need to grab a current link from: http://unity3d.com/get-unity/download/archive
 echo 'Downloading from https://unity3d.com/get-unity/download?thank-you=update&download_nid=48329&os=Mac: '
-curl -o Unity.pkg https://unity3d.com/get-unity/download?thank-you=update&download_nid=48329&os=Mac
+curl -o Unity.pkg 'https://unity3d.com/get-unity/download?thank-you=update&download_nid=48329&os=Mac'
 
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
